17b249766017d450d6aec99b4f14c724f23c38b3
minhduc1003 <vuminhduc231003@gmail.com>
2023-05-05 17:16:49 +0700

    fix sidebar 2

--- client/src/modules/dashboard/SideBar.jsx
+++ client/src/modules/dashboard/SideBar.jsx
@@ -10,17 +10,17 @@ const SideBar = ({ handleClick = () => {} }) => {
   const dispatch = useDispatch();
   const [dark, setDark] = useDarkMode();
   return (
-    <div className="w-full md:w-[72px] md:h-[733px] rounded-3xl shadow-sdprimary px-[14px] py-10 flex justify-center flex-col md:gap-10 flex-shrink-0 dark:shadow-darkShadow">
+    <div className="w-full  lg:w-[72px] lg:h-[733px] rounded-3xl shadow-sdprimary px-[14px] py-10 flex justify-center flex-col lg:gap-10 flex-shrink-0 dark:shadow-darkShadow">
       {sideBar.map((link) => {
         if (link.url === "/logout") {
           return (
             <Link
               onClick={() => dispatch(authLogOut())}
-              className="flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-xl text-icon-color  w-full p-5"
+              className="flex items-center gap-x-5 lg:w-12 lg:h-12 lg:justify-center lg:rounded-xl text-icon-color  w-full p-5"
               key={link.name}
             >
               <span>{link.icon}</span>
-              <span className="md:hidden">{link.name}</span>
+              <span className="lg:hidden">{link.name}</span>
             </Link>
           );
         }
@@ -28,10 +28,10 @@ const SideBar = ({ handleClick = () => {} }) => {
           return (
             <div
               onClick={() => setDark(!dark)}
-              className="mt-auto bg-white shadow-sdprimary dark:bg-darkSecondary dark:shadow-darkShadow flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-xl text-icon-color   w-full p-5"
+              className="mt-auto bg-white shadow-sdprimary dark:bg-darkSecondary dark:shadow-darkShadow flex items-center gap-x-5 lg:w-12 lg:h-12 lg:justify-center lg:rounded-xl text-icon-color   w-full p-5"
             >
               <span>{link.icon}</span>
-              <span className="md:hidden">{link.name}</span>
+              <span className="lg:hidden">{link.name}</span>
             </div>
           );
         }
@@ -43,12 +43,12 @@ const SideBar = ({ handleClick = () => {} }) => {
             to={link.url}
             className={({ isActive }) =>
               isActive
-                ? "flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-xl w-full p-5 text-primary bg-[#F1FBF7] dark:bg-darkSecondary "
-                : "flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-xl text-icon-color  w-full p-5 "
+                ? "flex items-center gap-x-5 lg:w-12 lg:h-12 lg:justify-center lg:rounded-xl w-full p-5 text-primary bg-[#F1FBF7] dark:bg-darkSecondary "
+                : "flex items-center gap-x-5 lg:w-12 lg:h-12 lg:justify-center lg:rounded-xl text-icon-color  w-full p-5 "
             }
           >
             <span>{link.icon}</span>
-            <span className="md:hidden">{link.name}</span>
+            <span className="lg:hidden">{link.name}</span>
           </NavLink>
         );
       })}
